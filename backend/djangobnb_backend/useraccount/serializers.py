ff77388f09b861bbe8d6f7f3f51bae85d46139f9
lowkeydo001 <edsel.lorejo@msunaawan.edu.ph>
2025-12-04 14:36:14 +0800

    video 5 fixed- MELVIN N. AMPARADO

--- backend/djangobnb_backend/useraccount/serializers.py
+++ backend/djangobnb_backend/useraccount/serializers.py
@@ -1,7 +1,16 @@
+from dj_rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
 
-
 from .models import User
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    name = serializers.CharField(max_length=100, required=False)
+
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data['name'] = self.validated_data.get('name', '')
+        return data
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
